e3a1dd0f76473d4d8af2232ba88ae85347b0a34b
welidlabidi <56021644+welidlabidi@users.noreply.github.com>
2020-04-27 17:10:20 +0200

    first commit

--- src/comp/trailer.js
+++ src/comp/trailer.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 const Trailer = (props) => {
-  let youtubeUrl = `https://www.youtube.com/embed/${props.trailer}`;
+  let url = `https://www.youtube.com/embed/${props.trailer}`;
   return (
     <div className="iframe">
       <embed
         style={{ width: "580px", height: "350px" }}
-        /*         frameBorder="0"
-         */ type="video/webm"
-        src={youtubeUrl}
+        frameBorder="0"
+        type="video/webm"
+        src={url}
       />
     </div>
   );
