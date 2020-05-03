b07504c77254280d5cab3c64ca509123e5c99d5f
welidlabidi <56021644+welidlabidi@users.noreply.github.com>
2020-05-03 18:25:27 +0200

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-
-# Movie
-Here you have a link to my Movie App https://welidlabidi.github.io/Movie/ <br>
-the app is made with reactjs with css and bootstrap <br>
-using hooks as useState and useEffect and axios as fetching api's
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Movie/precache-manifest.266f46bb5a77e31b47000535142c85c3.js"
+  "/Movie/precache-manifest.c4b58ed555aee81825ea9ed7b2067888.js"
 );
 
 self.addEventListener('message', (event) => {
